33d8f010c98328afe833224aa4baa8605d9444b5
RuKo <rusbko@gmail.com>
2017-03-28 16:58:09 +0700

    completed 3501

--- com/javarush/task/task35/task3501/GenericStatic.java
+++ com/javarush/task/task35/task3501/GenericStatic.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task35.task3501;
 
-public class GenericStatic {
+public class GenericStatic<T extends Number> {
     public static Object someStaticMethod(Object genericObject) {
         System.out.println(genericObject);
         return genericObject;
