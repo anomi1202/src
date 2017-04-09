df990b0f415f4a3ade57d4eccaf558bf59534e05
RuKo <rusbko@gmail.com>
2017-04-09 17:49:01 +0700

    Reflection & ClassLoader completed

--- com/javarush/task/task36/task3606/Solution.java
+++ com/javarush/task/task36/task3606/Solution.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task36.task3606;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,10 +30,59 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+        //формируем список файлов, состоящий только из классов в указанной dir
+        File[] files = new File(packageName + File.separator).listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".class");
+            }
+        });
+
+        //classLoader - будет считывать нашу ссылку на класс. С помощью него будем считывать и формировать class
+        ClassLoader classLoader = new ClassLoader() {
+            @Override
+            protected Class<?> findClass(String className) throws ClassNotFoundException {
+                byte[] classBytes = null;
+                try {
+                    classBytes = Files.readAllBytes(Paths.get(className));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return defineClass(null, classBytes, 0, classBytes.length);
+            }
+        };
+
+        for (File file : files) {
+            Class clazz = classLoader.loadClass(file.getAbsolutePath());
+            if (HiddenClass.class.isAssignableFrom(clazz))
+                hiddenClasses.add(clazz);
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
-        return null;
+        HiddenClass hiddenClass = null;
+        try{
+            for (Class clazz : hiddenClasses){
+                if (clazz.getSimpleName().toLowerCase().startsWith(key.toLowerCase())) {
+                    Constructor[] constructors = clazz.getDeclaredConstructors();
+                    for (Constructor constructor : constructors) {
+                        if (constructor.getParameterTypes().length == 0) {
+                            constructor.setAccessible(true);
+                            hiddenClass = (HiddenClass) constructor.newInstance();
+                        }
+                    }
+                }
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+        return hiddenClass;
     }
 }
 
