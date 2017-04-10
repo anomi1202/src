b520adc46f3a390af8d91b9c89ea6242eb69b3ef
RuKo <rusbko@gmail.com>
2017-04-10 08:03:51 +0700

    shortener 3310 big укорачиватель

---
+++ com/javarush/task/task33/task3310/Helper.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by Ru on 10.04.2017.
+ */
+public class Helper {
+    public static String generateRandomString(){
+
+    }
+}
---
+++ com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task33.task3310;
+
+
+/**
+ * Created by Ru on 09.04.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+}
