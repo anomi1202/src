609f55a1e391c5668c99d3cdc8d567856b9269ae
RuKo <rusbko@gmail.com>
2017-05-17 23:42:42 +0700

    completed 3913 big level39 without one task

--- com/javarush/task/task39/task3913/Solution.java
+++ com/javarush/task/task39/task3913/Solution.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task39.task3913;
 
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Solution {
-    public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
-        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
+    public static void main(String[] args) throws IOException, ParseException {
+        //LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        LogParser logParser = new LogParser(Paths.get("C:\\Users\\Ru\\Desktop\\JavaRushHomeWork\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs\\"));
+        System.out.println(logParser.getIPsForEvent(Event.LOGIN, null, null));
+
     }
+
+
 }
